2e6f37ff0489cb7c56e61ee61163afdf546b6a7a
HUA Yang <imhuay@163.com>
2018-04-28 10:15:33 +0800

    v3.0.2 tensorflow

--- huaytools/__init__.py
+++ huaytools/__init__.py
@@ -1,12 +1,10 @@
 __version__ = "0.3.2"
 
 # modules
-from . import utils
 from . import crawler
 from . import nlp
+from . import tensorflow as tf
 
 # utils
+# from . import utils
 from .utils import *
-
-# crawler
-from .crawler.tools import get_html
