3ae40170a191a8627930bbe220dbe7736d35c8bc
HUA Yang <imhuay@163.com>
2018-05-25 15:10:33 +0800

    add keras/utils

---
+++ huaytools/kreas/utils.py
@@ -0,0 +1,31 @@
+""""""
+
+
+class act:
+    """
+    Keras 中预定义的激活函数字符串
+    """
+    sigmoid = 'sigmoid'
+    relu = 'relu'
+
+
+class loss:
+    """
+    Keras 中预定义的损失函数字符串
+    """
+    binary_crossentropy = 'binary_crossentropy'
+
+
+class optimizer:
+    """
+    Keras 中预定义的优化器字符串
+    """
+    adam = "adam"
+
+
+class metrics:
+    """
+    Keras 中预定义的性能评估字符串
+    """
+    accuracy = 'accuracy'
+    acc = accuracy
